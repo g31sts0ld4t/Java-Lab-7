9bc8b7b3293647384dda77c86d65e20cc93e54af
Timo Gneiding <46979128+g31sts0ld4t@users.noreply.github.com>
2023-03-03 01:26:19 -0500

    Add files via upload

--- Lab7_Main.java
+++ Lab7_Main.java
@@ -19,7 +19,12 @@ public class Lab7_Main {
 		
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < (i + 1); j++) {
-				pw.write((j + 1) + "x" + (i + 1) + "=" + ((j + 1) * (i +1 )) + "\t");
+				int spaceSep = 4;
+				int longest = 6;
+				int spacing = 10;
+				String out1 = (j + 1) + "x" + (i + 1) + "=" + ((j + 1) * (i +1 )) + "\t";
+				String formatStr = "%-10s";
+				pw.write(String.format(formatStr, out1));
 			}
 			pw.write("\n");
 		}
